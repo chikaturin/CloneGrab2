9e58dc1d43fc37332f41de8319a1ff581b28943a
Chikaturin <ninhhoaithanh@gmail.com>
2024-04-20 00:07:54 +0700

    Edit tailwind PageList

--- src/ListStoreFood/ListStore.jsx
+++ src/ListStoreFood/ListStore.jsx
@@ -6,14 +6,14 @@ import { DataStore } from './DataListStore';
 
 const ItemHome = (prop) => {
     return (
-        <div className='col-md-3 col-sm-6 col-xs-12 px-2'>
+        <div className='px-2 mb-12'>
             <div className='Option_food'>
-                <img className='img_food' src={prop.image} alt="Food"></img>
-                <h5 className='namefood text-sm text-md text-lg'>{prop.title}</h5>
-                <div className='infor'>
-                    <span className='text-sm text-md text-lg'><FontAwesomeIcon icon={faStar} style={{color: "#FFD43B"}} />{prop.rate}</span>
-                    <span className='text-sm text-md text-lg'><FontAwesomeIcon icon={faClock} style={{color: "#000000"}} />{prop.time}</span>
-                    <span className='text-sm text-md text-lg'>•{prop.distance}</span>
+                <img className='img_food rounded-sm' src={prop.image} alt="Food"></img>
+                <h5 className='namefood text-xl font-bold min-h-10'>{prop.title}</h5>
+                <div className='infor grid grid-cols-3'>
+                    <span className=''><FontAwesomeIcon icon={faStar} style={{color: "#FFD43B"}} />{prop.rate}</span>
+                    <span className=''><FontAwesomeIcon icon={faClock} style={{color: "#000000"}} />{prop.time}</span>
+                    <span className=''>•{prop.distance}</span>
                 </div>
             </div>
         </div>
@@ -24,7 +24,33 @@ const ListStore = () => {
     return (
         <div className='body container'> 
             <div className='FindFood'><span><FontAwesomeIcon icon={faMagnifyingGlass} /> Tìm món ăn hoặc quán ăn</span></div>
-            <div className='Body_In_Home row justify-content-center'>
+            <div className='w-full justify-items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 px-4 my-8'>
+                <div className='w-full mb-3'>
+                    <span className='z-10 font-bold mt-16 ml-16 w-full mb-3  absolute '>Ở gần tôi</span>
+                    <img className='w-40 h- 36 sm:w-48 sm:h-40 opacity-45 relative' src="https://food.grab.com/static/images/placeholder-restaurant-2by1.jpg" alt="" />
+                </div>
+                <div className='w-full mb-3'>
+                    <span className='z-10 font-bold mt-16 ml-16 w-full mb-3  absolute '>Ở gần tôi</span>
+                    <img className='w-40 h- 36 sm:w-48 sm:h-40 opacity-45 relative' src="https://food.grab.com/static/images/placeholder-restaurant-2by1.jpg" alt="" />
+                </div>
+                <div className='w-full mb-3'>
+                    <span className='z-10 font-bold mt-16 ml-16 w-full mb-3  absolute '>Ở gần tôi</span>
+                    <img className='w-40 h- 36 sm:w-48 sm:h-40 opacity-45 relative' src="https://food.grab.com/static/images/placeholder-restaurant-2by1.jpg" alt="" />
+                </div>
+                <div className='w-full mb-3'>
+                    <span className='z-10 font-bold mt-16 ml-16 w-full mb-3  absolute '>Ở gần tôi</span>
+                    <img className='w-40 h- 36 sm:w-48 sm:h-40 opacity-45 relative' src="https://food.grab.com/static/images/placeholder-restaurant-2by1.jpg" alt="" />
+                </div>
+                <div className='w-full mb-3'>
+                    <span className='z-10 font-bold mt-16 ml-16 w-full mb-3  absolute '>Ở gần tôi</span>
+                    <img className='w-40 h- 36 sm:w-48 sm:h-40 opacity-45 relative' src="https://food.grab.com/static/images/placeholder-restaurant-2by1.jpg" alt="" />
+                </div>
+                <div className='w-full mb-3'>
+                    <span className='z-10 font-bold mt-16 ml-16 w-full mb-3  absolute '>Ở gần tôi</span>
+                    <img className='w-40 h- 36 sm:w-48 sm:h-40 opacity-45 relative' src="https://food.grab.com/static/images/placeholder-restaurant-2by1.jpg" alt="" />
+                </div>
+            </div>
+            <div className='Body_In_Home grid grid-cols-2 sm:grid-cols-4'>
                     {DataStore.map((item,index)=>(
                             <ItemHome 
                                 image={item.image} 
