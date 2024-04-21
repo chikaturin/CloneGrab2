176d9899ffb774918ed233d3b8c4c428e87bb54d
Chikaturin <ninhhoaithanh@gmail.com>
2024-04-21 10:58:14 +0700

    fished tailwind

--- src/PageDetailFood/DetailFood.jsx
+++ src/PageDetailFood/DetailFood.jsx
@@ -1,11 +1,11 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { Datahome } from '../HomePage/data';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
-import { faStar, faClock ,faCirclePlus} from '@fortawesome/free-solid-svg-icons'; 
+import { faStar, faClock, faCirclePlus } from '@fortawesome/free-solid-svg-icons'; 
 import OrderFood from './OrderFood';
-import { Menudata } from './menudetail';
+import { Menudata } from './menudetaildata';
 import { ChooseMenu } from './dataChooseMenu';
 
 const ItemDetail = (prop) => {
@@ -14,198 +14,136 @@ const ItemDetail = (prop) => {
             <h1 className='text-5xl font-bold'>{prop.title}</h1>
             <p className='my-2'>Cơm,...</p>
             <div className='grid grid-cols-3 w-1/4'>
-                <span className=''><FontAwesomeIcon icon={faStar} style={{color: "#FFD43B"}} />{prop.rate}</span>
-                <span className=''><FontAwesomeIcon className='mr-2' icon={faClock} style={{color: "#000000"}} />{prop.time}</span>
+                <span className=''><FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B" }} />{prop.rate}</span>
+                <span className=''><FontAwesomeIcon className='mr-2' icon={faClock} style={{ color: "#000000" }} />{prop.time}</span>
                 <span className=''>•{prop.distance}</span>
             </div>
         </div>
     );
 };
+
 const ItemMenu = (props) => {
     return (
-        <div className='menuitem'>
-            <div className='item_detail_list'>
-            <img className='img_detail' src={props.image}></img>
-                <div className='infor_menu'>
-                    <p className='name_dish'>{props.name_detail}</p>
+        <div className='menuitem mx-2 px-4 py-4 my-4 rounded bg-white'>
+            <div className='item_detail_list grid grid-cols-2'>
+                <img className='img_detail rounded' src={props.image} alt={props.name_detail} />
+                <div className='infor_menu pl-2'>
+                    <p className='name_dish text-xl mb-3'>{props.name_detail}</p>
                     <p className='description'>{props.decription}</p>
-                    <p className='price'>{props.price}<span className='add'><FontAwesomeIcon icon={faCirclePlus} style={{color: "#01B04E",}} /></span></p>
+                    <p className='price font-bold'>{props.price}<span className='add float-right'><FontAwesomeIcon icon={faCirclePlus} style={{ color: "#01B04E", width: '34px', height: '34px' }} /></span></p>
                 </div>
             </div>
         </div>
     );
 };
 
-const DetailFood =() => {
+const DetailFood = () => {
     const [scrollPosition, setScrollPosition] = useState(0);
-    useEffect(() => {
-      const handleScroll = () => {
-        const position = window.scrollY;
-        setScrollPosition(position);
-      };
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    },);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [colors, setColors] = useState(['#01B04E', 'black', 'black', 'black', 'black', 'black']); 
     const [isVisible, setIsVisible] = useState(false);
+    
     const toggleVisibility = () => {
         setIsVisible(!isVisible);
       };
-    const [colors, setColors] = useState(['#01B04E', 'black', 'black', 'black', 'black', 'black']); 
-    const [underline, setUnderlines] = useState(['#01B04E 3px solid', 'none', 'none', 'none', 'none', 'none']); 
+    useEffect(() => {
+        const handleScroll = () => {
+            const position = window.scrollY;
+            setScrollPosition(position);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        const scrollThresholds = [500, 1800,2800,3800,5300,6800];
+        let newIndex = 0;
+
+        for (let i = 0; i < scrollThresholds.length; i++) {
+            if (scrollPosition >= scrollThresholds[i]) {
+                newIndex = i;
+            }
+        }
+
+        setActiveIndex(newIndex);
+    }, [scrollPosition]);
 
     const handleClick = (index) => {
-    const newColors = Array(6).fill('black'); 
-    newColors[index] = '#01B04E';
-
-    const newUnderline = Array(6).fill('none'); 
-    newUnderline[index]= '#01B04E 3px solid'; 
-    setColors(newColors);
-    setUnderlines(newUnderline);
+        const newColors = Array(6).fill('black'); 
+        newColors[index] = '#01B04E';
+        setColors(newColors);
     };
     return (
-            <div className='body'>
-                    <OrderFood toggleVisibility={toggleVisibility} isVisible={isVisible}/>
-                    <div className='body_menu'>
-                        <div className='infor_store container '>
-                            <div className='container w-full'>
-                                {Datahome.map((item, index) => {
-                                    if (item.id === 1) {
-                                        return (
-                                            <ItemDetail
-                                                key={index}
-                                                image={item.image}
-                                                title={item.title}
-                                                rate={item.rate}
-                                                time={item.time}
-                                                distance={item.distance}
-                                            />
-                                        );
-                                    }
-                                    return null;
-                                })}
-                                <p style={{marginLeft:10}}>Giờ mở cửa <span className='ml-6'>Hôm nay 10:00-20:00</span></p>
-                            </div>
-                        </div>
+        
+        <div className='body'>
+            <OrderFood toggleVisibility={toggleVisibility} isVisible={isVisible}/>
 
-                            <div style={{ backgroundColor: scrollPosition > 130 ? 'white' : 'transparent',position: scrollPosition > 130 ? 'fixed' : 'static'}} className='w-full top-9 h-fit min-h-32 block sm:hidden'>
-                                <div className="container">
-                                    <div className=' container w-full grid my-4 grid-cols-6 text-center translate translate-y-1/2'>
-                                        {ChooseMenu.map((item,index) => (
-                                            index=item.id,
-                                            <a className='pb-3' href={'#x'+item.id} key={index} style={{ color: colors[index - 1], borderBottom: `${underline[index - 1]}`}} onClick={() => handleClick(index - 1)}>{item.Title}</a>
-                                            
-                                        ))}
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div style={{ backgroundColor: scrollPosition > 730 ? 'white' : 'transparent',position: scrollPosition > 730 ? 'fixed' : 'static'}} className='w-full top-20 h-fit min-h-48 sm:min-h-0 hidden sm:block'>
-                                <div className="container">
-                                    <div className=' container w-full grid my-4 grid-cols-6 text-center translate translate-y-1/2'>
-                                        {ChooseMenu.map((item,index) => (
-                                            index=item.id,
-                                            <a className='pb-3' href={'#x'+item.id} key={index} style={{ color: colors[index - 1], borderBottom: `${underline[index - 1]}`}} onClick={() => handleClick(index - 1)}>{item.Title}</a>
-                                        ))}
-                                    </div>
-                                </div>
-                            </div>
-
-                        <div className='bg-gray-100' id='x1'>
-                            <div className="container ">
-                                <p style={{marginTop:10,marginBottom:50, marginLeft:10}}>Menu2</p>
-                                <div className='row' onClick={toggleVisibility}>
-                                {Menudata.map((item,index)=>(
-                                        <ItemMenu 
-                                            key={item.id} 
-                                            image={item.image} 
-                                            name_detail={item.name_detail} 
-                                            decription={item.decription} 
-                                            price={item.price}/>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                        <div className='bg-gray-100'id='x2'>
-                            <div className="container ">
-                                <p style={{marginTop:10,marginBottom:50, marginLeft:10}}>Menu</p>
-                                <div className='row' onClick={toggleVisibility}>
-                                {Menudata.map((item,index)=>(
-                                        <ItemMenu 
-                                            key={item.id} 
-                                            image={item.image} 
-                                            name_detail={item.name_detail} 
-                                            decription={item.decription} 
-                                            price={item.price}/>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                        <div className='bg-gray-100'id='x3'>
-                            <div className="container ">
-                                <p style={{marginTop:10,marginBottom:50, marginLeft:10}}>Menu</p>
-                                <div className='row' onClick={toggleVisibility}>
-                                {Menudata.map((item,index)=>(
-                                        <ItemMenu 
-                                            key={item.id} 
-                                            image={item.image} 
-                                            name_detail={item.name_detail} 
-                                            decription={item.decription} 
-                                            price={item.price}/>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                        <div className='bg-gray-100'id='x4'>
-                            <div className="container ">
-                                <p style={{marginTop:10,marginBottom:50, marginLeft:10}}>Menu</p>
-                                <div className='row' onClick={toggleVisibility}>
-                                {Menudata.map((item,index)=>(
-                                        <ItemMenu 
-                                            key={item.id} 
-                                            image={item.image} 
-                                            name_detail={item.name_detail} 
-                                            decription={item.decription} 
-                                            price={item.price}/>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                        <div className='bg-gray-100'id='x5'>
-                            <div className="container ">
-                                <p style={{marginTop:10,marginBottom:50, marginLeft:10}}>Menu</p>
-                                <div className='row' onClick={toggleVisibility}>
-                                {Menudata.map((item,index)=>(
-                                        <ItemMenu 
-                                            key={item.id} 
-                                            image={item.image} 
-                                            name_detail={item.name_detail} 
-                                            decription={item.decription} 
-                                            price={item.price}/>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                        <div className='bg-gray-100'id='x6'>
-                            <div className="container ">
-                                <p style={{marginTop:10,marginBottom:50, marginLeft:10}}>Menu</p>
-                                <div className='row' onClick={toggleVisibility}>
-                                {Menudata.map((item,index)=>(
-                                        <ItemMenu 
-                                            key={item.id} 
-                                            image={item.image} 
-                                            name_detail={item.name_detail} 
-                                            decription={item.decription} 
-                                            price={item.price}/>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-
+            <div className='body_menu'>
+                <div className='infor_store container'>
+                    <div className='container w-full'>
+                        {Datahome.map((item, index) => {
+                            if (item.id === 1) {
+                                return (
+                                    <ItemDetail
+                                        key={index}
+                                        image={item.image}
+                                        title={item.title}
+                                        rate={item.rate}
+                                        time={item.time}
+                                        distance={item.distance}
+                                    />
+                                );
+                            }
+                            return null;
+                        })}
+                        <p style={{ marginLeft: 10 }}>Giờ mở cửa <span className='ml-6'>Hôm nay 10:00-20:00</span></p>
                     </div>
+                </div>
+
+                <div className='w-full top-9 h-fit min-h-32 block sm:hidden' style={{ backgroundColor: scrollPosition > 130 ? 'white' : 'transparent', position: scrollPosition > 130 ? 'fixed' : 'static' }}>
+                    <div className="container">
+                        <div className='container w-full grid my-4 grid-cols-6 text-center translate translate-y-1/2'>
+                            {ChooseMenu.map((item, index) => (
+                                <a className='pb-3' href={`#x${item.id}`} key={index} style={{ color: colors[index], borderBottom: index === activeIndex ? '3px solid #01B04E' : 'none', color: index === activeIndex ? '#01B04E' : 'black' }} onClick={() => handleClick(index)}>{item.Title}</a>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                <div className='w-full top-20 h-fit min-h-48 sm:min-h-0 hidden sm:block' style={{ backgroundColor: scrollPosition > 730 ? 'white' : 'transparent', position: scrollPosition > 730 ? 'fixed' : 'static' }}>
+                    <div className="container">
+                        <div className='container w-full grid my-4 grid-cols-6 text-center translate translate-y-1/2'>
+                            {ChooseMenu.map((item, index) => (
+                                <a className='pb-3' href={`#x${item.id}`} key={index} style={{ color: colors[index], borderBottom: index === activeIndex ? '3px solid #01B04E' : 'none', color: index === activeIndex ? '#01B04E' : 'black' }} onClick={() => handleClick(index)}>{item.Title}</a>
+                                ))}
+                        </div>
+                    </div>
+                </div>
+
+                <div className='bg-gray-100'>
+                    <div className='container'>
+                        {ChooseMenu.map((item, index) => (
+                            <div id={`x${item.id}`} key={index} style={{ paddingTop: scrollPosition > 750 ? '230px' : '10px' }}>
+                                <p className=' text-4xl font-bold ml-4' style={{ marginLeft: "22px" }} key={index}>{item.Title}</p>
+                                <div className='bg-gray-100'>
+                                    <div className="container mt-16">
+                                        <div className='grid grid-cols-1 sm:grid-cols-3' onClick={toggleVisibility}>
+                                            {Menudata.map((item, index) => (
+                                                <ItemMenu key={item.id} image={item.image} name_detail={item.name_detail} decription={item.decription} price={item.price} />
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
-        );
-    };
+        </div>
+    );
+};
+
 export default DetailFood;
--- src/PageDetailFood/OrderFood.jsx
+++ src/PageDetailFood/OrderFood.jsx
@@ -4,12 +4,14 @@ import {faX} from '@fortawesome/free-solid-svg-icons';
 
 const ItemMenu = (props) => {
     return (
-        <div className='item_detail_order row'>
-            <img className='img_detail_order col-md-6 col-sm-12' src={props.image}></img>
-            <div className='infor_menu_order col-md-6 col-sm-12'>
-                <h3 className='name_dish_order'>{props.name_detail}</h3>
-                <p className='description_order'>{props.decription}</p>
-                <p className='price_order'>{props.price}</p>
+        <div className='grid grid-cols-2 p-4'>
+            <img className='rounded-t-md rounded-b-md' src={props.image}></img>
+            <div className='pl-2'>
+                <h3 className='text-4xl font-bold'>{props.name_detail}</h3>
+                <div className='grid grid-cols-1 sm:grid-cols-2 mt-4'>
+                    <p className='text-sm text-gray-500'>{props.decription}</p>
+                    <p className=' text-right text-xl font-bold'>{props.price}</p>
+                </div>
             </div>
         </div>
     );
@@ -39,38 +41,41 @@ const OrderFood = ({ toggleVisibility, isVisible }) => {
     if(isVisible)
     {
         return (
-            <div className='Body_orderFood row'>
-                <div className="detail_order_black col-md-7" onClick={toggleVisibility}>
-                </div>
-                <div className="detail_Orderfood col-md-5">
-                    <p className='out' onClick={toggleVisibility}><FontAwesomeIcon icon={faX} /></p>
-                    <div className="infor_detail_order">
+                <div className='grid grid-cols-1 overflow-y-scroll sm:grid-cols-2 fixed top-0 left-0 w-full overflow-hidden h-full z-50'>
+                    <div className="bg-black opacity-15 hidden sm:block" onClick={toggleVisibility}></div>
+                    <div className="bg-white">
+                    <p className='p-4 border border-bottom container' onClick={toggleVisibility}><FontAwesomeIcon icon={faX} /></p>
+                    <div className=" border-b-8 border-gray-200">
                         <ItemMenu 
                                 image={"https://food-cms.grab.com/compressed_webp/merchants/5-C4EKLZN2CBEUUE/hero/faff5dbaf7024a2c98f33303920f8ef1_1688956019240286277.webp"} 
                                 name_detail={"Cơm Gà Đùi Luộc"} 
                                 decription={"Gà Ta . Ăn Kèm với Mắm Cà + Salat trộn . Đặc biệt kèm theo muối tiêu chanh pha sẵn"} 
                                 price={"63.000"}/>
                     </div>
-                    <div className="add_option_order mt-4">
-                        <h3 style={{fontSize:20}}>Thêm</h3>
-                            <div className="form-check">
-                            <input className='form-check-input' type="checkbox" name="tea" value="tea"/>
-                            <label className='form-check-label' > Trà tắc <span>7.000</span></label><br/>
-                            <input className='form-check-input' type="checkbox" name="tea" value="tea"/>
-                            <label className='form-check-label'> Trà tắc <span>7.000</span></label><br/>
-                            <input className='form-check-input' type="checkbox" name="tea" value="tea"/>
-                            <label className='form-check-label'> Trà tắc <span>7.000</span></label><br/>
-                            <input className='form-check-input' type="checkbox" name="tea" value="tea"/>
-                            <label className='form-check-label'> Trà tắc <span>7.000</span></label><br/>
+                    <div className="pl-6 mt-4 ">
+                        <h3 className='text-xl font-bold'>Thêm</h3>
+                            <div className="form-check py-4 w-full">
+                                <input className='form-check-input text-2xl' type="checkbox" name="tea" value="tea"/>
+                                <div className='mt-1 px-4 grid grid-cols-2'><span className='text-lg mt-1'>Trà Tắc</span><span className='text-right w-full'>7.000</span></div><br/>
+                                <input className='form-check-input text-2xl' type="checkbox" name="tea" value="tea"/>
+                                <div className='mt-1 px-4 grid grid-cols-2'><span className='text-lg mt-1'>Trà Tắc</span><span className='text-right w-full'>7.000</span></div><br/>
+                                <input className='form-check-input text-2xl' type="checkbox" name="tea" value="tea"/>
+                                <div className='mt-1 px-4 grid grid-cols-2'><span className='text-lg mt-1'>Trà Tắc</span><span className='text-right w-full'>7.000</span></div><br/>
+                                <input className='form-check-input text-2xl' type="checkbox" name="tea" value="tea"/>
+                                <div className='mt-1 px-4 grid grid-cols-2'><span className='text-lg mt-1'>Trà Tắc</span><span className='text-right w-full'>7.000</span></div><br/>
+                                <input className='form-check-input text-2xl' type="checkbox" name="tea" value="tea"/>
+                                <div className='mt-1 px-4 grid grid-cols-2'><span className='text-lg mt-1'>Trà Tắc</span><span className='text-right w-full'>7.000</span></div><br/>
+                                <input className='form-check-input text-2xl' type="checkbox" name="tea" value="tea"/>
+                                <div className='mt-1 px-4 grid grid-cols-2'><span className='text-lg mt-1'>Trà Tắc</span><span className='text-right w-full'>7.000</span></div><br/>
                             </div>
                     </div>
-                    <div className="pay container row">
-                        <div className="count d-flex col-md-3 col-sm-12">
-                            <button className='decrease' onClick={decrementCount}>-</button>
-                            <div className='valuecount'>{count}</div>
-                            <button className='increase' onClick={incrementCount}>+</button>
+                    <div className="pay container my-4 grid grid-cols-1 sm:grid-cols-2">
+                        <div className="count d-flex col-md-3 col-sm-12 mb-4 translate translate-x-1/2 ">
+                            <button className=' border p-1 px-3 rounded' onClick={decrementCount}><span className='text-xl text-blue-500'>-</span></button>
+                            <div className='valuecount p-3 px-4'>{count}</div>
+                            <button className=' border p-1 px-3 rounded' onClick={incrementCount}><span className='text-xl text-blue-500'>+</span></button>
                         </div>
-                        <button className="button_pay px-2 col-md-9 col-sm-12">{content}</button>
+                        <button className="button_pay px-2 bg-green-600 rounded"><span className='text-white'>{content}</span></button>
                     </div>
                 </div>
             </div>
