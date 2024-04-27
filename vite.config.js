21c450d98a0b94c7fd3d95dfcbda1db14a5ef559
Chikaturin <ninhhoaithanh@gmail.com>
2024-04-27 17:29:18 +0700

    micro 1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,24 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+
+      name: "microFE1",
+      filename:"remoteEntry.js",
+      remotes: {},
+      exposes: {
+        "./Header":"./src/Header",
+        "./Footer":"./src/Footer",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 })
